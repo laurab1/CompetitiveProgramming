23e06a842b05ce98e4cd5c75a0e244cc68f30b8b
laurab1 <l.bussi1@studenti.unipi.it>
2020-04-08 11:50:23 +0200

    rewrite missing_number

--- missing_num.cpp
+++ missing_num.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include <vector>
 
 typedef struct test_struct {
     int size;
-    int* array;
+    std::vector<int> vec;
 } test_t;
 
 //prints the missing element in a given array
 void print_missing(test_t test) {
-    int array_sum = 0;
+    int vec_sum = test.vec.front();
     int sum = 0;
-    for(int i=0; i<test.size-1; i++)
-        array_sum = array_sum + test.array[i];
-    sum = (test.size*(test.size+1))/2;
-    cout << sum - array_sum << endl;
+    for(auto it = test.vec.begin()+1; it != test.vec.end(); it++)
+        vec_sum = vec_sum + *it;
+    sum = ((test.size+2)*(test.size+1))/2;
+    std::cout << sum - vec_sum << std::endl;
 }
 
 int main() {
-    int t = 0; //number of test cases
-    cin >> t;
-    test_t* tests = (test_t*) malloc(t*sizeof(test_t)); //array of test objects
-    for(int i=0; i<t; i++) {
-        cin >> tests[i].size;
-        tests[i].array = (int*) malloc((tests[i].size-1)*sizeof(int)); //each array contains size-1 integers
-        for(int j=0; j<tests[i].size-1; j++)
-            cin >> tests[i].array[j];
+    int num_tests = 0; //number of test cases
+    std::cin >> num_tests;
+    std::vector<test_t> tests(num_tests);
+    for(auto it = tests.begin(); it != tests.end(); it++) {
+        std::cin >> it->size;
+        it->size--;
+        int el;
+        for(int i=0; i<it->size; i++) {
+            std::cin >> el;
+            it->vec.push_back(el);
+        }
     }
-    for(int i=0; i<t; i++) {
-        print_missing(tests[i]);
-        free(tests[i].array);
-    }
-    free(tests);
+    for(auto it = tests.begin(); it != tests.end(); it++) 
+        print_missing(*it);
 
     return 0;
 }
