5e7a2a7a747c7850fb1dc9bcca2613e6429eb266
laurab1 <l.bussi1@studenti.unipi.it>
2020-09-06 22:53:32 +0200

    fix tree and queries

--- 28-10-19/tree_and_queries.cpp
+++ 28-10-19/tree_and_queries.cpp
@@ -14,51 +14,37 @@ struct query {
 
 using tree = std::vector<std::vector<int64_t>>;
 int64_t block_size;
-std::vector<int64_t> stimes;
-std::vector<int64_t> ftimes;
-std::vector<bool> visited;
+std::vector<int64_t> flatten;
 std::vector<int64_t> start;
 std::vector<int64_t> end;
-int64_t occurr[10000];
-int64_t occurrk[10000];
-int64_t t = 0;
+int64_t occurr[100001];
+int64_t occurrk[100001];
 int64_t d = 0;
 int64_t b = 0;
 
-void dfs(std::vector<query> &queries, std::vector<int64_t> &flatten, tree &adj, int64_t idx) {
-    if(visited.at(idx))
-        return;
-    flatten.at(t) = idx;
-    start.at(idx) = ++b;
-    stimes.at(idx) = t++;
-    visited.at(idx) = true;
-    for(auto u : adj.at(idx))
-        dfs(queries, flatten, adj, u);
-    flatten.at(t) = idx;
-    end.at(idx) = b;
-    ftimes.at(idx) = t++;
+void dfs(std::vector<int64_t> &colors, std::vector<int64_t> &flatten, tree &adj, int64_t idx, int64_t old) {
+    start[idx] = ++b;
+    flatten[b] = colors[idx];
+    for(auto u : adj[idx])
+        if(u != old)
+            dfs(colors, flatten, adj, u, idx);
+    end[idx] = b;
 }
 
-void add(std::vector<int64_t> &colors, int64_t idx) {
-    occurr[colors[idx]]++;
-    occurrk[occurr[colors[idx]]]++;
+void add(int64_t idx) {
+    occurr[flatten[idx]]++;
+    occurrk[occurr[flatten[idx]]]++;
 }
 
-void remove(std::vector<int64_t> &colors, int64_t idx) {
-    occurrk[occurr[colors[idx]]]--;
-    occurr[colors[idx]]--;
+void remove(int64_t idx) {
+    occurrk[occurr[flatten[idx]]]--;
+    occurr[flatten[idx]]--;
 }
 
 void tree_and_queries(std::vector<int64_t> &colors, tree &adj, std::vector<query> &queries) {
     int64_t n = colors.size();
     int64_t m = queries.size();
-    std::vector<int64_t> flatten(n*2);
-    stimes.resize(n);
-    ftimes.resize(n);
-    visited.resize(n);
-    start.resize(n);
-    end.resize(n);
-    block_size = sqrt(n);
+    block_size = (int64_t)sqrt(n);
     std::vector<int64_t> results(m);
 
     std::sort(queries.begin(), queries.end(), [](query a, query b) {
@@ -67,37 +53,30 @@ void tree_and_queries(std::vector<int64_t> &colors, tree &adj, std::vector<query
         return a.r < b.r;
     });
 
-    dfs(queries, flatten, adj, 0);
-    for(int64_t i = 0; i < m; i++) {
-        int64_t j = queries.at(i).l;
-        queries.at(i).l = start[j];
-        queries.at(i).r = end[j] - 1;
-    }
-
-    int64_t l = 0;
-    int64_t r = 0;
+    int64_t l = queries[0].l;
+    int64_t r = l - 1;
 
     for(int64_t i = 0; i < m; i++) {
         int64_t lq = queries[i].l;
         int64_t rq = queries[i].r;
 
         while(l < lq) {
-            remove(colors, flatten.at(l));
+            remove(l);
             l++;
         }
 
         while(l > lq) {
-            add(colors, flatten.at(l-1));
             l--;
+            add(l);
         }
         
-        while(r <= rq) {
-            add(colors, flatten.at(r));
+        while(r < rq) {
             r++;
+            add(r);
         }
 
-        while(r > rq + 1) {
-            remove(colors, flatten.at(r-1));
+        while(r > rq) {
+            remove(r);
             r--;
         }
 
@@ -111,26 +90,35 @@ void tree_and_queries(std::vector<int64_t> &colors, tree &adj, std::vector<query
 int main() {
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
+
     int64_t n, m;
     std::cin >> n >> m;
-    std::vector<int64_t> colors(n);
+    std::vector<int64_t> colors(n+1);
+    flatten.resize(n+1);
+    start.resize(n);
+    end.resize(n);
     tree adj(n);
 
-    for(int64_t i = 0; i < n; i++)
-        std::cin >> colors.at(i);
+    for(int64_t i = 0; i < n; i++) {
+        std::cin >> colors[i];
+    }
+
     for(int64_t i = 0; i < n - 1; i++) {
         int64_t s, d;
         std::cin >> s >> d;
-        adj.at(s-1).push_back(d-1);
+        adj[s-1].push_back(d-1);
+        adj[d-1].push_back(s-1);
     }
+    dfs(colors, flatten, adj, 0, -1);
+    
     std::vector<query> queries(m);
     for(int64_t i = 0; i < m; i++) {
         int64_t q1, q2;
         std::cin >> q1 >> q2;
-        queries.at(i).l = q1 - 1;
-        queries.at(i).r = q2;
-        queries.at(i).i = i;
-        queries.at(i).c = q2;
+        queries[i].l = start[q1-1];
+        queries[i].r = end[q1-1];
+        queries[i].i = i;
+        queries[i].c = q2;
     }
     tree_and_queries(colors, adj, queries);
 }
